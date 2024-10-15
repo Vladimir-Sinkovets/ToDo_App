e7a1eddae07b88741e443500819dbf5c26c217b3
Vladimir Sinkovets <vovasin49@gmail.com>
2024-10-15 20:28:55 +0300

    add create command

---
+++ UseCases/Handlers/ToDo/Commands/CreateToDo/CreateToDoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace UseCases.Handlers.ToDo.Commands.CreateToDo
+{
+    public class CreateToDoCommand : IRequest
+    {
+        public string Title {  get; set; }
+    }
+}
---
+++ UseCases/Handlers/ToDo/Commands/CreateToDo/CreateToDoCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Interfaces.DataAccess;
+using MediatR;
+using ToDoApp.Entities.Enums;
+using ToDoApp.Entities.Models;
+
+namespace UseCases.Handlers.ToDo.Commands.CreateToDo
+{
+    public class CreateToDoCommandHandler : IRequestHandler<CreateToDoCommand>
+    {
+        private readonly IDbContext _dbContext;
+
+        public CreateToDoCommandHandler(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(CreateToDoCommand request, CancellationToken cancellationToken)
+        {
+            var todoEntry = new ToDoEntry()
+            {
+                Created = DateOnly.FromDateTime(DateTime.Now),
+                Status = Status.Active,
+                Title = request.Title,
+            };
+
+            _dbContext.ToDos.Add(todoEntry);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
