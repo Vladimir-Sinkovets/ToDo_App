236b98127419ce02dcdd30297f585540b1a5dac5
Vladimir Sinkovets <vovasin49@gmail.com>
2024-10-15 20:30:27 +0300

    add complete command

---
+++ UseCases/Handlers/ToDo/Commands/CompleteToDo/CompleteToDoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace UseCases.Handlers.ToDo.Commands.CompleteToDo
+{
+    public class CompleteToDoCommand : IRequest
+    {
+        public Guid ToDoId { get; set; }
+    }
+}
---
+++ UseCases/Handlers/ToDo/Commands/CompleteToDo/CompleteToDoCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Interfaces.DataAccess;
+using MediatR;
+using ToDoApp.Entities.Enums;
+using UseCases.Exceptions;
+
+namespace UseCases.Handlers.ToDo.Commands.CompleteToDo
+{
+    public class CompleteToDoCommandHandler : IRequestHandler<CompleteToDoCommand>
+    {
+        private readonly IDbContext _dbContext;
+
+        public CompleteToDoCommandHandler(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(CompleteToDoCommand request, CancellationToken cancellationToken)
+        {
+            var toDoEntry = _dbContext.ToDos.FirstOrDefault(t => t.Id == request.ToDoId);
+
+            if (toDoEntry == null)
+                throw new ToDoNotFoundException($"ToDo with id = {request.ToDoId} does not exist");
+
+            toDoEntry.Status = Status.Completed;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
