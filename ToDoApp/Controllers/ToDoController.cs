e80b3a17cab9ea4e7c088693f8ea7a6a9f6639a4
Vladimir Sinkovets <vovasin49@gmail.com>
2024-10-15 22:46:55 +0300

    add adding action method

---
+++ ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.ViewModels;
+using MediatR;
+using UseCases.Handlers.ToDo.Commands.CreateToDo;
+using UseCases.Handlers.ToDo.Queries.GetToDoCollectionByStatus;
+using UseCases.Enums;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ToDoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddViewModel viewModel)
+        {
+            var command = new CreateToDoCommand()
+            {
+                Title = viewModel.Title,
+            };
+
+            await _mediator.Send(command);
+
+            return View();
+        }
+    }
+}
